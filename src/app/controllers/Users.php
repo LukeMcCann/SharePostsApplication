8c0581a223f45a8e47a79e259868bd1d08dced99
LukeMcCann <developer-lukemccann@outlook.com>
2020-11-23 19:39:43 +0000

    Fix validation issue

--- src/app/controllers/Users.php
+++ src/app/controllers/Users.php
@@ -7,7 +7,52 @@ class Users extends Controller {
     }
 
     public function register() {
-        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'name' => trim($_POST['name']),
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'confirm_password' => trim($_POST['confirm_password']),
+                'name_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+                'confirm_password_err' => ''
+            ];
+
+            // TODO: Separate into validation class
+            if (empty($data['email'])) {
+                $data['email_err'] = 'Please enter a valid email.';
+            }
+
+            if (empty($data['name'])) {
+                $data['name_err'] = 'Please enter a username.';
+            }
+
+            if (empty($data['password'])) {
+                $data['password_err'] = 'Please enter a valid password.';
+            } elseif (strlen($data['password']) < 6) {
+                $data['password_err'] = 'Password must be at least 6 characters in length.';
+            }
+
+            if (empty($data['confirm_password'])) {
+                $data['confirm_password_err'] = 'Please confirm password!';
+            } else {
+                if ($data['password'] !== $data['confirm_password']) {
+                    $data['confirm_password_err'] = 'Passwords must match!';
+                }
+            }
+
+            if (empty($data['email_err']) && empty($data['name_err']) 
+            && empty($data['password_err']) && empty($data['confirm_password_err'])) {
+                // Validation Succeeds
+                die('Success');
+            } else {
+                // Validation Fails - display errors
+                $this->view('users/register', $data);
+            }
 
         } else {
             $data = [
