d55301f76aa15157c0a24dd1fea1065f11d7b0e9
LukeMcCann <developer-lukemccann@outlook.com>
2020-11-23 22:42:39 +0000

    Add posts display model data

--- src/app/controllers/Posts.php
+++ src/app/controllers/Posts.php
@@ -1,10 +1,24 @@
 <?php 
 class Posts extends Controller
 {
+    private $postModel;
+
+    public function __construct()
+    {
+        if (!isLoggedIn()) {
+            redirect('/users/login');
+        }
+
+        $this->postModel = $this->model('Post');
+    }
     public function index() 
     {
-        $data = [];
-        
+        $posts = $this->postModel->getPosts();
+
+        $data = [
+            'posts' => $posts
+        ];
+
         $this->view('posts/index', $data);
     }
 }
